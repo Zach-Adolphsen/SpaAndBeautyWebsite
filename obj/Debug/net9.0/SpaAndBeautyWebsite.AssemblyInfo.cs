db99e0174f7bb1d85d882a81749fb492e40528af
dylanrattai <dylan.rattai@ndus.edu>
2025-12-06 15:07:20 -0600

    Fixed alignment

--- obj/Debug/net9.0/SpaAndBeautyWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/SpaAndBeautyWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaAndBeautyWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443b7c9d40b5bdbf242da67dae0207c84509b060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee49e1d2df9c7f6e11a18aa0317c676eeeed7806")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaAndBeautyWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaAndBeautyWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
